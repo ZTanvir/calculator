b2ba21648ac21dfaadb65888501ed2e1c3db6fcf
zahirul <zahirulopel@gmail.com>
2022-08-27 10:33:03 +0600

    Add four core function add,subtract,divide,multiply in app.js
    Created structure of the calculator project by adding index.html and
    app.js . Added four core function for calculate number.

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+/*
+ * Basic calculator calculation function
+ */
+let add = (number1, number2) => number1 + number2;
+console.log(add(10, 20));
+let subtract = (number1, number2) => number1 - number2;
+console.log(subtract(10, 20));
+let multiply = (number1, number2) => number1 * number2;
+console.log(multiply(10, 20));
+let divide = (number1, number2) => number1 / number2;
+console.log(divide(100, 33).toFixed(2));
+
+
+
