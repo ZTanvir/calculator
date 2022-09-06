1bf07a7a3f7f5faae58b0cc2008211525d7e5872
zahirul <zahirulopel@gmail.com>
2022-09-06 10:53:58 +0600

    Solved calculation problem ex-12+7-5*3 = 42 and 12+7= 19

--- scripts/app.js
+++ scripts/app.js
@@ -4,14 +4,18 @@ let resultScreenEl = document.querySelector(".calculate-result");
 
 // Store the number when user clicked it and
 // If user not select a symbol
-let numberOne = 0;
+let numberOne = null;
 let storeNumerOne = "";
 // Store the symbol when it is clicked
 let symbol = "";
 // Store the number when user clicked it and
 // when user select a symbol
-let numberTwo = 0;
+let numberTwo = null;
 let storeNumerTwo = "";
+
+// Number to store pair calculation 5 * 5 = 25
+let total = null;
+
 /*
  * Basic calculator calculation function
  */
@@ -45,14 +49,16 @@ function displayNumberOnClick(e) {
     let itemTextContent = e.target.textContent;
     // Show Number after symbol(On calululator display)
     processScreenEl.textContent += itemTextContent;
-    storeNumerOne += itemTextContent;
-    numberTwo = Number(storeNumerOne);
+    storeNumerTwo += itemTextContent;
+    numberTwo = Number(storeNumerTwo);
+    console.log("InputNumberTwo:", numberTwo);
   } else {
     let itemTextContent = e.target.textContent;
     // Show Number before symbol(On calululator display)
     processScreenEl.textContent += itemTextContent;
-    storeNumerTwo += itemTextContent;
-    numberOne = Number(storeNumerTwo);
+    storeNumerOne += itemTextContent;
+    numberOne = Number(storeNumerOne);
+    console.log("InputNumberOne:", numberOne);
   }
 }
 
@@ -72,7 +78,39 @@ function storeSymbol(e) {
   let symbolIs = e.target.textContent;
   // Show symbol on processScreenEl(On calululator display)
   processScreenEl.textContent += symbolIs;
-  symbol = symbolIs;
+  if (symbol == "") {
+    symbol = symbolIs;
+  }
+
+  // calculate in pair
+  if (symbol === "+" || symbol === "-" || symbol === "/" || symbol === "*") {
+    if (total != null) {
+      console.log("InSymbolSymbol:", symbol);
+      console.log("InSymbolNumberTwo", numberTwo);
+      let answer = operate(symbol, total, numberTwo);
+      total = answer;
+      console.log("Total:", answer);
+      resultScreenEl.textContent = total;
+      storeNumerTwo = "";
+      symbol = symbolIs;
+      
+      console.log("End");
+    }
+    // when number1 and number2 has number in it
+    else if (numberOne != null && numberTwo != null) {
+      console.log("InSymbolNumone:", numberOne);
+      console.log("InSymbolSymbol:", symbol);
+      console.log("InSymbolNumberTwo", numberTwo);
+      let answer = operate(symbol, numberOne, numberTwo);
+      total = answer;
+      resultScreenEl.textContent = total;
+      console.log("Total:", answer);
+      storeNumerOne = "";
+      storeNumerTwo = "";
+      symbol = symbolIs;
+      console.log("End");
+    }
+  }
 }
 
 allSymbolarr.forEach((item) => item.addEventListener("click", storeSymbol));
@@ -80,12 +118,34 @@ allSymbolarr.forEach((item) => item.addEventListener("click", storeSymbol));
 let equalBtnEl = document.querySelector(".equal");
 function calculateEqual(e) {
   if (symbol === "+" || symbol === "-" || symbol === "/" || symbol === "*") {
-    console.log("Yes");
-    console.log("NumberOne:", numberOne);
-    console.log("Symbol:", symbol);
-    console.log("NumberTwo", numberTwo);
-  } else {
-    console.log("NO");
+    if (total != null) {
+      console.log("Totalbefore:", numberOne);
+      console.log("Symbol:", symbol);
+      console.log("NumberTwo", numberTwo);
+      let answer = operate(symbol, total, numberTwo);
+      total = answer;
+      resultScreenEl.textContent = total;
+      total = null;
+      numberTwo = null;
+      storeNumerOne = "";
+      storeNumerTwo = "";
+      symbol = "";
+      processScreenEl.textContent = "";
+      console.log("Total:", answer);
+    } else if (numberOne != null && numberTwo != null) {
+      let answer = operate(symbol, numberOne, numberTwo);
+      resultScreenEl.textContent = answer;
+      console.log("Numberone:", numberOne);
+      console.log("Symbol:", symbol);
+      console.log("NumberTwo", numberTwo);
+      numberOne = null;
+      numberTwo = null;
+      storeNumerOne = "";
+      storeNumerTwo = "";
+      symbol = "";
+      processScreenEl.textContent = "";
+      
+    }
   }
 }
 equalBtnEl.addEventListener("click", calculateEqual);
